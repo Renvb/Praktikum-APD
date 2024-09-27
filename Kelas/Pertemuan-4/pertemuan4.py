b86b114de28f6874b6803135422d4049d9176813
Razib | Ren <138003158+Renvb@users.noreply.github.com>
2024-09-27 14:57:55 +0800

    pertemuan 4

---
+++ Kelas/Pertemuan-4/pertemuan4.py
@@ -0,0 +1,68 @@
+# ulang = 10
+# for i in range(ulang):
+#     # print("Perulangan ke-"+str(i))
+#     print(f"Perulangan ke-{i} kali")
+
+# for i in range(1, 11, 2):
+#     print(f"Perulangan ke-{i} kali")
+
+# simpan = [12, "udin petot", 14.5, True, 'A']
+# for i in simpan:
+#     print(i)
+
+# for i in range(1, 4):
+#     for j in range(1, 4):
+#         print(f"{i} x {j} = {i * j}")
+#     print()
+
+# for i in range(1, 6, 2):
+#     for j in range(1, 6, 2):
+#         print(f"{i} x {j} = {i * j}")
+#     print()
+
+# for i in range(11, 0, -1):
+#     print(i)
+
+# ulang = "ya"
+# hitung = 0
+# while ulang == "ya":
+#     print(f"Perulangan ke-{hitung} kali")
+#     ulang = input("Apakah Anda masih ingin mengulang? ")
+#     hitung += 1
+# print("Perulangan selesai")
+
+# x = 0
+# while x < 5:
+#     print(x)
+#     x += 1
+
+# hitung = 1
+# while True:
+#     ulang = input("Apakah Anda masih ingin mengulang? ")
+#     if ulang == "tidak":
+#         break
+#     hitung += 1
+
+# print(f"Total perulangan: {hitung}")
+
+# for i in range(10):
+#     if i % 2 == 0:
+#         continue
+#     if i == 5:
+#         break
+#     print(i)
+
+# usn = "tes"
+# pw = "123"
+# salah = 0
+
+# while salah < 3:
+#     username = input("Masukkan username: ")
+#     password = input("Masukkan password: ")
+#     if username == usn and password == pw:
+#         print("Login Berhasil")
+#         break
+#     else:
+#         print("Login Gagal")
+#         salah += 1
+
