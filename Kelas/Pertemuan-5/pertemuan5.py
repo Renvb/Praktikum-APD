e41a42b8bcc58d97509c573c1d4827c8dff62713
Razib | Ren <138003158+Renvb@users.noreply.github.com>
2024-10-04 15:23:49 +0800

    pertemuan 5

---
+++ Kelas/Pertemuan-5/pertemuan5.py
@@ -0,0 +1,71 @@
+# list = ["a", 
+#         2, 
+#         True, 
+#         [1, 2, 3]]
+
+# tas = ["buku", 32, True, 3.14, ["IF", 24]]
+
+# for i in tas:
+#     print(i)
+
+# for i in range(len(tas)):
+#     print(tas[i])
+
+# matkul = ["Kalkulus", "Fisika Dasar", "Pengantar Teknologi Informasi"]
+# print(matkul)
+
+# matkul.append("Logika Informatika")
+# print(matkul)
+
+# matkul.insert(1, "Matematika Diskrit")
+# print(matkul)
+
+# matkul[1] = "fisika kuantum"
+# print(matkul)
+
+# del matkul[1]
+# print(matkul)
+
+# buang = matkul.pop(1)
+# print(matkul)
+# print(buang)
+
+# prodi = ["Informatika", "Sistem Informasi", "Teknik Arsitektur", 
+#         "Teknik Lingkungan", "Teknik Pertambangan", "Teknik Elektro",
+#         "Teknik Industri", "Teknik SIpil", "Teknik Geologi", "Teknik Kimia"]
+
+# print(prodi[::2], "\n", prodi)
+
+# bensin = ["avanza", "honda", "yamaha"]
+# listrik = ["tesla", "SAIC"]
+# semua = bensin+listrik
+# print(semua)
+# print(bensin*3)
+
+# barang = [["sepatu", "tas", "kemeja"], ["pulpen", "buku", "pensil"]]
+# print(barang[1][1])
+
+# for i in barang:
+#     for j in i:
+#         print(j)
+
+# perabotan = []
+# print(perabotan)
+# perabotan.append("kursi")
+# print(perabotan)
+
+# nama = ("Razib Ramadhan", "ren", "rencchi")
+# print(nama)
+
+# mahasiswa = (69, "Informatika", "2209106044", "Aldy septian ")
+# absen, prodi, nim, nama = mahasiswa
+
+# print(absen)
+# print(prodi)
+# print(nim)
+# print(nama)
+
+# mahasiswa = (69, "Informatika", "2209106044", "Aldy septian ")
+# mahasiswa = list(mahasiswa)
+# print(mahasiswa)
+# print(type(mahasiswa))
